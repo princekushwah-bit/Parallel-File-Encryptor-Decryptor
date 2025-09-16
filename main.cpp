8f2facc0a5dbac643678d2503d9161238daf3bd9
prince kushwah <princecse362@gmail.com>
2025-09-16 19:06:43 +0530

    main.cpp

--- main.cpp
+++ main.cpp
@@ -10,35 +10,38 @@ using std::string;
 int main(int argc, char *argv[]){
     string directory;
     string action;
-    std::cout<<"'Enter the directory path: "<<std::endl;
+
+    std::cout<<"Enter the directory path: "<<std::endl;
     std::getline(std::cin, directory);
-     std::cout<<"'Enter the action (encrypt/decrypt): "<<std::endl;
+
+    std::cout<<"Enter the action (encrypt/decrypt): "<<std::endl;
     std::getline(std::cin, action);
 
     try {
-       if(fs::exists(directory) && fs::is_directory(directory)) {
-         ProcessManagement processManagement;
-         for(const auto &entry: fs::recursive_directory_iterator(directory)){
-            if(entry.is_regular_file()){
-                string filePath = entry.path().string();
-                IO io(filePath);
-                std::fstream fileStream = std::move(io.getFileStream());
-                if(fileStream.is_open()){
-                  Action taskAction = (action == "encrypt") ? Action::ENCRYPT : Action::DECRYPT;
-                  auto task = std::make_unique<Task>(std::move(fileStream), taskAction, filePath);
-                  processManagement.submitToQueue(std::move(task));
-                } else {
-                  std::cout<<"Error: Could not open file: "<<filePath<<std::endl;
+        if(fs::exists(directory) && fs::is_directory(directory)) {
+            ProcessManagement processManagement;
+
+            for(const auto &entry: fs::recursive_directory_iterator(directory)){
+                if(entry.is_regular_file()){
+                    string filePath = entry.path().string();
+                    IO io(filePath);
+                    std::fstream fileStream = std::move(io.getFileStream());
+
+                    if(fileStream.is_open()){
+                        Action taskAction = (action == "encrypt") ? Action::ENCRYPT : Action::DECRYPT;
+                        auto task = std::make_unique<Task>(std::move(fileStream), taskAction, filePath);
+                        processManagement.submitToQueue(std::move(task));
+                    } else {
+                        std::cout<<"Error: Could not open file: "<<filePath<<std::endl;
+                    }
                 }
             }
-         }
-         processManagement.executeTasks();
-       } else {
-          std::cout<<"Error: Directory does not exist: "<<directory<<std::endl;
-       }
+
+            processManagement.executeTasks();
+        } else {
+            std::cout<<"Error: Directory does not exist: "<<directory<<std::endl;
+        }
     } catch(const fs::filesystem_error &e) {
-      std::cout<<"FileSystem Error: "<<e.what()<<std::endl;
-
+        std::cout<<"FileSystem Error: "<<e.what()<<std::endl;
     }
-
 }
