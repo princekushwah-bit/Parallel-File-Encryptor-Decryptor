94a165a012a51bc97e200bca5e4e6ec74fea2af7
prince kushwah <princecse362@gmail.com>
2025-09-16 19:04:12 +0530

    Update ProcessManagement.hpp

--- src/app/processes/ProcessManagement.hpp
+++ src/app/processes/ProcessManagement.hpp
@@ -4,16 +4,28 @@
 #include "Task.hpp"
 #include <queue>
 #include <memory>
+#include <mutex>
+#include <condition_variable>
+#include <thread>
+#include <vector>
+#include <atomic>
 
-class ProcessManagement
-{
+class ProcessManagement {
 public:
-    ProcessManagement();
+    ProcessManagement(size_t numThreads = std::thread::hardware_concurrency());
+    ~ProcessManagement();
+
     bool submitToQueue(std::unique_ptr<Task> task);
     void executeTasks();
 
 private:
+    void workerThread();
+
     std::queue<std::unique_ptr<Task>> taskQueue;
+    std::mutex queueMutex;
+    std::condition_variable cv;
+    std::vector<std::thread> workers;
+    std::atomic<bool> stop;
 };
 
 #endif
